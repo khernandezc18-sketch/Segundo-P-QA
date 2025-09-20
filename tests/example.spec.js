e7c35199df5a1691d9492eaafd4ff30ab9615556
khernandezc18-sketch <khernandezc18@miumg.edu.gt>
2025-09-19 22:30:57 -0600

    feat: proyecto Playwright + CI con reportes

---
+++ tests/example.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('Verificar título de Playwright', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
